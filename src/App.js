54f534d9d39cf02278d0a9b878f9325228ed24bb
Joshua Lehman <joshlehman.dev@gmail.com>
2024-07-12 17:35:48 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Thumbnail from './components/Thumbnail';
+import { items, itemImages } from './items';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {items.map((item) => (
+        <Thumbnail key={item.itemId} image={itemImages[item.itemId]} title={item.title} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Thumbnail.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+import './Thumbnail.css';
+
+function Thumbnail({ image, title }) {
+  return (
+    <a href="#todo" className="thumbnail-component">
+      <div>
+        <img src={image} alt={title} />
+      </div>
+      <p>{title}</p>
+    </a>
+  );
+}
+
+Thumbnail.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Thumbnail;
