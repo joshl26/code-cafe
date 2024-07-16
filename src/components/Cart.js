858da1dc8b8c21d3d5b9ef7dc577019a404b6e0b
Joshua Lehman <joshlehman.dev@gmail.com>
2024-07-15 22:16:36 -0400

    updates

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,50 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import ItemType from '../types/item';
 import './Cart.css';
 import CartRow from './CartRow';
 
 function Cart({ cart, items, dispatch }) {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [zipCode, setZipCode] = useState('');
+
   const subTotal = cart.reduce((acc, item) => {
     const detailItem = items.find((i) => i.itemId === item.id.itemId);
     const itemPrice = detailItem.salePrice ?? detailItem.price;
     return item.quantity * itemPrice + acc;
   }, 0);
+
+  const taxPercentage = parseInt(zipCode.substring(0, 1) || '0', 10) + 1;
+  const taxRate = taxPercentage / 100;
+  const tax = subTotal * taxRate;
+  const total = subTotal + tax;
+  const isFormValid = zipCode.length === 5 && name.trim();
+
+  console.log('tax: ', tax);
+
+  const submitOrder = (event) => {
+    event.preventDefault();
+    console.log('name: ', name);
+    console.log('phone: ', phone);
+    console.log('zipcode: ', zipCode);
+  };
+
+  const setFormattedPhone = (newNumber) => {
+    const digits = newNumber.replace(/\D/g, '');
+    let formatted = digits.substring(0, 3);
+    if (digits.length === 3 && newNumber[3] === '-') {
+      formatted = `${formatted}-`;
+    } else if (digits.length > 3) {
+      formatted = `${formatted}-${digits.substring(3, 6)}`;
+    }
+    if (digits.length === 6 && newNumber[7] === '-') {
+      formatted = `${formatted}`;
+    } else if (digits.length > 6) {
+      formatted = `${formatted}-${digits.substring(6, 10)}`;
+    }
+    setPhone(formatted);
+  };
   // console.log(cart);
   // console.log({ items });
 
@@ -36,6 +72,42 @@ function Cart({ cart, items, dispatch }) {
             Subtotal: $
             {subTotal.toFixed(2)}
           </div>
+          <div>
+            Tax: $
+            {tax.toFixed(2)}
+          </div>
+          {zipCode.length === 5 ? (
+            <>
+              <div>
+                Tax: $
+                {tax.toFixed(2)}
+              </div>
+              <div>
+                Total: $
+                {total.toFixed(2)}
+              </div>
+            </>
+          ) : (
+            <div className="warning">Enter Zip Code to get total</div>
+          )}
+          <h2>Checkout</h2>
+          <form onSubmit={submitOrder}>
+            <label htmlFor="name">
+              Name
+              <input id="name" type="text" value={name} onChange={(event) => setName(event.target.value)} required />
+            </label>
+            <label htmlFor="phone">
+              Phone Number
+              <input id="phone" type="tel" value={phone} onChange={(event) => setFormattedPhone(event.target.value)} />
+            </label>
+            <label htmlFor="zipcode" value={zipCode} onChange={(event) => setZipCode(event.target.value)}>
+              Zip Code
+              <input id="zipcode" type="text" maxLength="5" inputMode="numeric" required />
+            </label>
+            <button type="submit" disabled={!isFormValid}>
+              Order Now
+            </button>
+          </form>
         </>
       )}
     </div>
